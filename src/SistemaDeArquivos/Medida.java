f9ccf8cb866f0159b8afbe1d25a85614784c33bf
LPatiele <lo.patiele@gmail.com>
2016-04-11 14:32:01 -0300

    inicio

---
+++ src/SistemaDeArquivos/Medida.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaDeArquivos;
+
+/**
+ *
+ * @author Jonatha Varjão
+ */
+public class Medida {
+    private String sigla;
+    private String descricao;
+}
---
+++ src/SistemaDeArquivos/Produto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemaDeArquivos;
+
+/**
+ *
+ * @author Jonatha Varjão
+ */
+public class Produto {
+    private int idProduto;
+    private String descricao;
+    private float saldo;
+    private float precoUnitario;
+    private Medida siglaUnidadeMedida;
+    
+}
